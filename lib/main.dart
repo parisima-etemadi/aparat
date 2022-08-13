52832602b375da8ae003f802cdcf9d9a4111161e
Parisima <parisimaetemadi9675@gmail.com>
2022-08-13 09:34:48 +0430

    feature:disable Landscape mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:aparat/data/repositories/video_repository.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'data/data provider/Video_data_provider.dart';
 import 'ui/search/pages/search_result_page.dart';
 import 'injection_container.dart' as di;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   di.init();
   runApp(const MyApp());
 }
